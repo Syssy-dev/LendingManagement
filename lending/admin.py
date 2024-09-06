64636b71472c467fa068c0e0c168c8e8dc10ca15
SEBOROK <rapdpx@gmail.com>
2024-09-06 13:16:24 +0200

    ajout de l'app lending sur l'admin

--- lending/admin.py
+++ lending/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Lending
+
 
 # Register your models here.
+@admin.register(Lending)
+class LendingAdmin(admin.ModelAdmin):
+    list_display = ('id', 'user_email', 'book_id', 'date_borrowed', 'date_due')
+    list_filter = ('date_borrowed', 'date_due')
+    search_fields = ('user_email', 'book_id')
+    ordering = ('-date_borrowed',)
--- lending/models.py
+++ lending/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+from django.db import models
+from django.contrib.auth.models import User
+from django.utils import timezone
+
+class Lending(models.Model):
+    user_email = models.EmailField()  # Champ pour stocker l'adresse e-mail de l'utilisateur
+    book_id = models.PositiveIntegerField()  # Assuming book_id is an integer. Adjust type if necessary.
+    date_borrowed = models.DateTimeField(default=timezone.now)  # date d'emprunt
+    date_due = models.DateTimeField()  # date d'échéance
+
+    def __str__(self):
+        return f"Lending {self.id} - User: {self.user.email}, Book ID: {self.book_id}"
