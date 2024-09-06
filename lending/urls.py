7d6fb170df6974d3f47f7d2c500db04b8f1889a5
SEBOROK <rapdpx@gmail.com>
2024-09-06 14:42:56 +0200

    requirement

--- lending/urls.py
+++ lending/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import create_lending
+from .views import create_lending, expired_lendings
 
 urlpatterns = [
     path('create-lending/', create_lending, name='create_lending'),
+    path('expired-lendings/', expired_lendings, name='expired_lendings'),
 ]
--- lending/views.py
+++ lending/views.py
@@ -6,6 +6,8 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from .models import Lending
 from .serializers import LendingSerializer
+from django.utils import timezone
+
 
 @api_view(['POST'])
 def create_lending(request):
@@ -14,3 +16,11 @@ def create_lending(request):
         serializer.save()
         return Response(serializer.data, status=status.HTTP_201_CREATED)
     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+@api_view(['GET'])
+def expired_lendings(request):
+    now = timezone.now()
+    expired_lendings = Lending.objects.filter(date_due__lt=now)  # Filtrer tous les objets où la date_due est passée
+    serializer = LendingSerializer(expired_lendings, many=True)  # Sérialiser les objets filtrés
+    return Response(serializer.data)  # Retourner la liste des objets sérialisés
